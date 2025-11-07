c0abf909fbf0bffac2fdf858842b22598506bf11
Aakash <aakash.singh@bugendaitech.com>
2025-11-07 11:25:22 +0530

    added hono server

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import { reactRouterHonoServer } from "react-router-hono-server/dev"; // add this
 import tsconfigPaths from "vite-tsconfig-paths";
 import devtoolsJson from "vite-plugin-devtools-json";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths(), devtoolsJson(),mkcert()],
+  plugins: [
+    devtoolsJson(),
+    reactRouterHonoServer(),
+    reactRouter(),
+    tsconfigPaths(),
+    mkcert(),
+  ],
 });
