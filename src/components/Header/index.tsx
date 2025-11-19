3255f71c83f0a7a4b16da41d54459aa8ca455777
PrasadBugendaitech <prasad.birari@bugendaitech.com>
2025-11-19 22:03:04 +0530

    change header UI for mobile view

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,6 +10,7 @@ import {
   Menu,
   Avatar,
   Text,
+  Stack,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import classes from "./index.module.css";
@@ -40,37 +41,34 @@ const Header = () => {
   return (
     <Box className={classes.sticky} mb={10}>
       <header className={classes.header}>
-        <Group justify="space-between" h="100%">
-          <Link to="/">
+        <Group justify="space-between" h="100%" gap="xs" wrap="nowrap">
+          <Link to="/" style={{ display: "flex", alignItems: "center" }}>
             <Image
               src="/assets/snaptosell-light.png"
-              alt=""
+              alt="SnapToSell Logo"
               className={classes.headerLogo}
             />
           </Link>
 
-          <Group h="100%" gap={0} visibleFrom="md">
+          <Group h="100%" gap={0} visibleFrom="md" wrap="nowrap">
             <Link to="/" className={classes.link}>
               Home
             </Link>
-
             <Link to="/features" className={classes.link}>Features</Link>
             <Link to="/how-it-works" className={classes.link}>How It Works</Link>
             <Link to="/scam-database" className={classes.link}>Scam Database</Link>
             <Link to="/waitlist" className={classes.link}>Join Waitlist</Link>
-      
           </Group>
 
-
-          <Group visibleFrom="md">
+          <Group gap="xs" visibleFrom="md" wrap="nowrap">
             {isLoggedIn ? (
-              <Menu shadow="md" width={200}>
+              <Menu shadow="md" width={200} position="bottom-end">
                 <Menu.Target>
-                  <Group gap="xs" style={{ cursor: "pointer" }}>
+                  <Group gap="xs" style={{ cursor: "pointer" }} wrap="nowrap">
                     <Avatar size="sm" radius="xl">
                       {session.user.name?.charAt(0).toUpperCase() || session.user.email?.charAt(0).toUpperCase()}
                     </Avatar>
-                    <Text size="sm" fw={500}>
+                    <Text size="sm" fw={500} visibleFrom="lg">
                       {session.user.name || session.user.email}
                     </Text>
                   </Group>
@@ -87,10 +85,10 @@ const Header = () => {
               </Menu>
             ) : (
               <>
-                <Button variant="default" component={Link} to={"/sign-in"}>
+                <Button variant="default" component={Link} to={"/sign-in"} size="sm">
                   Sign In
                 </Button>
-                <Button component={Link} to={"/register"}>
+                <Button component={Link} to={"/register"} size="sm">
                   Register
                 </Button>
               </>
@@ -101,6 +99,7 @@ const Header = () => {
             opened={drawerOpened}
             onClick={toggleDrawer}
             hiddenFrom="md"
+            size="sm"
           />
         </Group>
       </header>
@@ -110,60 +109,96 @@ const Header = () => {
         onClose={closeDrawer}
         size="100%"
         padding="md"
-        title="Navigation"
-        hiddenFrom="sm"
+        title={
+          <Group gap="xs">
+            <Image
+              src="/assets/snaptosell-light.png"
+              alt="SnapToSell"
+              w={100}
+            />
+          </Group>
+        }
+        hiddenFrom="md"
         zIndex={1000000}
       >
-        <ScrollArea h="calc(100vh - 80px" mx="-md">
-          {/*<Divider my="sm" />
+        <ScrollArea h="calc(100vh - 100px)" mx="-md" px="md">
+          <Stack gap="xs" mt="md">
+            <Link to="/" className={classes.mobileLink} onClick={closeDrawer}>
+              Home
+            </Link>
+            <Link to="/features" className={classes.mobileLink} onClick={closeDrawer}>
+              Features
+            </Link>
+            <Link to="/how-it-works" className={classes.mobileLink} onClick={closeDrawer}>
+              How It Works
+            </Link>
+            <Link to="/scam-database" className={classes.mobileLink} onClick={closeDrawer}>
+              Scam Database
+            </Link>
+            <Link to="/waitlist" className={classes.mobileLink} onClick={closeDrawer}>
+              Join Waitlist
+            </Link>
+          </Stack>
 
-           <Link to="/" className={classes.link}>
-            Home
-          </Link>
-          <Link to="/features" className={classes.link}>
-            Features
-          </Link>
-          <Link to="/how-it-works" className={classes.link}>
-            How It Works
-          </Link>
-          <Link to="/scam-database" className={classes.link}>
-            Scam Database
-          </Link>
-          <Link to="/waitlist" className={classes.link}>
-            Join Waitlist
-          </Link> */}
+          <Divider my="md" />
 
-          {/* <Link to="/feature" className={classes.link}>Features</Link>
-          <Link to="/howItWorks" className={classes.link}>How It Works</Link>
-          <Link to="/scamDatabase" className={classes.link}>Scam Database</Link>
-          <Link to="/waitlist" className={classes.link}>Join Waitlist</Link>
-
-          <Divider my="sm" /> */}
-
-          <Group justify="center" grow pb="xl" px="md">
-            {isLoggedIn ? (
-              <Group justify="center" w="100%">
-                <Avatar size="sm" radius="xl">
+          {isLoggedIn ? (
+            <Stack gap="md" mt="md">
+              <Group gap="sm" p="sm" style={{ borderRadius: 8, backgroundColor: "var(--mantine-color-gray-0)" }}>
+                <Avatar size="md" radius="xl">
                   {session.user.name?.charAt(0).toUpperCase() || session.user.email?.charAt(0).toUpperCase()}
                 </Avatar>
-                <Text size="sm" fw={500}>
-                  {session.user.name || session.user.email}
-                </Text>
-                <Button variant="default" onClick={handleSignOut} fullWidth>
-                  Sign Out
-                </Button>
+                <div style={{ flex: 1, minWidth: 0 }}>
+                  <Text size="sm" fw={500} truncate>
+                    {session.user.name || session.user.email}
+                  </Text>
+                  <Text size="xs" c="dimmed" truncate>
+                    {session.user.email}
+                  </Text>
+                </div>
               </Group>
-            ) : (
-              <>
-                <Button variant="default" component={Link} to={"/sign-in"}>
-                  Sign In
-                </Button>
-                <Button component={Link} to={"/register"}>
-                  Register
-                </Button>
-              </>
-            )}
-          </Group>
+              <Button
+                component={Link}
+                to="/detect"
+                fullWidth
+                onClick={closeDrawer}
+                mb="xs"
+              >
+                Dashboard
+              </Button>
+              <Button
+                variant="light"
+                color="red"
+                fullWidth
+                onClick={() => {
+                  handleSignOut();
+                  closeDrawer();
+                }}
+              >
+                Sign Out
+              </Button>
+            </Stack>
+          ) : (
+            <Stack gap="sm" mt="md">
+              <Button
+                variant="default"
+                component={Link}
+                to={"/sign-in"}
+                fullWidth
+                onClick={closeDrawer}
+              >
+                Sign In
+              </Button>
+              <Button
+                component={Link}
+                to={"/register"}
+                fullWidth
+                onClick={closeDrawer}
+              >
+                Register
+              </Button>
+            </Stack>
+          )}
         </ScrollArea>
       </Drawer>
     </Box>
