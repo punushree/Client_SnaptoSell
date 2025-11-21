c72ab9a71d52bfff76918bb891f8505303de64db
Aakash <aakash.singh@bugendaitech.com>
2025-11-21 18:35:16 +0530

    updates

--- src/lib/server/auth/index.ts
+++ src/lib/server/auth/index.ts
@@ -2,21 +2,20 @@ import { betterAuth } from "better-auth";
 import { createPool } from "mysql2/promise";
 
 export const auth = betterAuth({
-    // database: mikroOrmAdapter(await getOrm()),
-    database: createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        waitForConnections: true,
-        connectionLimit: 10,
-        maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-        idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-        queueLimit: 0,
-        enableKeepAlive: true,
-        keepAliveInitialDelay: 0,
-    }),
-    emailAndPassword: {
-        enabled: true,
-    },
+  database: createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
 });
