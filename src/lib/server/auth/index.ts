52e7a344efc397eff9401646943076c2fed95f54
Aakash <aakash.singh@bugendaitech.com>
2025-11-21 20:43:47 +0530

    session cache update

--- src/lib/server/auth/index.ts
+++ src/lib/server/auth/index.ts
@@ -18,4 +18,10 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // Cache duration in seconds
+    },
+  },
 });
